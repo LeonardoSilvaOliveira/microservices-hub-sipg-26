4d3cfe5eb77bbab45785b3846adb9d97bc3da482
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-15 10:14:09 -0300

    add eureka server, and config ms-pedidos and ms-pagamentos

--- ms-pagamentos/src/main/java/br/com/fiap/ms_pagamentos/MsPagamentosApplication.java
+++ ms-pagamentos/src/main/java/br/com/fiap/ms_pagamentos/MsPagamentosApplication.java
@@ -2,8 +2,10 @@ package br.com.fiap.ms_pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsPagamentosApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class MsPagamentosApplication {
 	}
 
 }
+
